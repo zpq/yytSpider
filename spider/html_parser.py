618296c3e86018274ee16a1b2d8f180efd1f3840
zpq <307154456@qq.com>
2016-01-06 18:00:56 +0800

    add a new crawl way

--- spider/html_parser.py
+++ spider/html_parser.py
@@ -43,8 +43,22 @@ class HtmlParser(object):
         new_urls = self._get_new_urls(page_url, soup)
         new_datas = self._get_new_datas(page_url, soup)
         return new_urls, new_datas
-       
+
+    def parse_single_page(self, page_url, html_content):
+        if html_content is None or page_url is None:
+            return
+        soup = BeautifulSoup(html_content, 'html.parser', from_encoding = 'utf-8')
+        res_datas = {}
+        #<dd class="lemmaWgt-lemmaTitle-title"><h1>PHP</h1>
+        title_node = soup.find('dd', class_='lemmaWgt-lemmaTitle-title').find('h1')
+        res_datas['title'] = title_node.get_text()
         
+        #<div class="lemma-summary" label-module="lemmaSummary">
+        summary_node = soup.find('div', class_='lemma-summary')
+        res_datas['summary'] = summary_node.get_text()
+        
+        res_datas['url'] = page_url
+        return res_datas
         
     
     
--- spider/spider_main.py
+++ spider/spider_main.py
@@ -14,24 +14,47 @@ class SpiderMain(object):
         self.parses = html_parser.HtmlParser()
         self.outputer = html_outputer.HtmlOutputer()
 
-    
+#给予一个初始页面和最大抓取数量，抓取所有在页面上出现的链接的页面title与sammary    
+#     def crawl(self, root_url):
+#         if root_url is None:
+#             return
+#         self.urls.add_new_url(root_url)
+#         count = 1;
+#         while self.urls.has_new_url():
+#             try:
+#                 new_url = self.urls.get_new_url()
+#                 print "crawl %d : %s" %(count, new_url)
+#                 html_content = self.downloader.download(new_url)
+#                 new_urls, new_datas = self.parses.parse(new_url, html_content)
+#                 self.urls.add_new_urls(new_urls)
+#                 self.outputer.collect_datas(new_datas)
+#                 if count == 100:
+#                     break
+#                 count = count + 1
+#             except:
+#                 print "crawl failed"
+#         self.outputer.output_html()
+
+
+#抓取给定页面的title与summary和所有子(不含孙)链接的title与summary
     def crawl(self, root_url):
         if root_url is None:
             return
-        self.urls.add_new_url(root_url)
+        html_content = self.downloader.download(root_url)
+        new_urls, new_datas = self.parses.parse(root_url, html_content)
+        self.outputer.collect_datas(new_datas)
+        self.urls.add_new_urls(new_urls)
         count = 1;
         while self.urls.has_new_url():
             try:
                 new_url = self.urls.get_new_url()
                 print "crawl %d : %s" %(count, new_url)
                 html_content = self.downloader.download(new_url)
-                new_urls, new_datas = self.parses.parse(new_url, html_content)
-                self.urls.add_new_urls(new_urls)
+                new_datas = self.parses.parse_single_page(new_url, html_content)
                 self.outputer.collect_datas(new_datas)
-                if count == 100:
-                    break
                 count = count + 1
-            except:
+            except Exception,e:
+                print e
                 print "crawl failed"
         self.outputer.output_html()
             
